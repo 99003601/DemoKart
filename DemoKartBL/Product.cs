f64fe9b8b0291e04af40e4712bdbc8f4a19ff06d
99003601 <78849903+99003601@users.noreply.github.com>
2021-02-25 01:35:04 +0530

    Update Product.cs

--- DemoKartBL/Product.cs
+++ DemoKartBL/Product.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace DemoKartBL
 {
-    class Product
+    public class Product
     {
+        public int productId;
+        public String productName;
+        public int quantity;
+        public double price;
+        //Category categoryObj = new Category();
+
+        public Product(string productName)
+        {
+            productId = 0;
+            productName = null;
+            quantity = 0;
+            price = 0;
+        }
+        public Product(int productId, String productName, double price, int quantity)
+        {
+            this.productId = productId;
+            this.productName = productName;
+            this.quantity = quantity;
+            this.price = price;
+        }
     }
 }
